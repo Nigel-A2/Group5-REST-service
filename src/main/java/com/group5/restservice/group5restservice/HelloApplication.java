38082a689ec644de23a1e9de779319b992f768aa
NigelOOSD <nigel.awrey@edu.sait.ca>
2022-04-09 21:59:00 -0500

    Initial commit

---
+++ src/main/java/com/group5/restservice/group5restservice/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.group5.restservice.group5restservice;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
