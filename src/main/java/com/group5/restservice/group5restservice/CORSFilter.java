5f4228e28e73a2273a2b3a76de91cbcfd875dca5
NigelOOSD <nigel.awrey@edu.sait.ca>
2022-04-15 15:33:55 -0500

    Added CORS bypass

---
+++ src/main/java/com/group5/restservice/group5restservice/CORSFilter.java
@@ -0,0 +1,20 @@
+package com.group5.restservice.group5restservice;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
+        containerRequestContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        containerRequestContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        containerRequestContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        containerRequestContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        containerRequestContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+    }
+}
+
