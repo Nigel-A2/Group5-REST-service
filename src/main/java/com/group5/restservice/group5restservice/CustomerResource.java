4964d3105765895502549293b050415eb6d0c6d2
NigelOOSD <nigel.awrey@edu.sait.ca>
2022-04-15 11:00:38 -0500

    Added database connection

--- src/main/java/com/group5/restservice/group5restservice/CustomerResource.java
+++ src/main/java/com/group5/restservice/group5restservice/CustomerResource.java
@@ -4,8 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path("/hello-world")
-public class HelloResource {
+@Path("/customer")
+public class CustomerResource {
     @GET
     @Produces("text/plain")
     public String hello() {
